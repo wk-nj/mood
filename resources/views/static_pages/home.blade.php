834ab71d991cb03f8feffc926d07b459cc34a8e7
wk-nj <15380314863@163.com>
2018-05-09 15:44:33 +0800

    layout

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
-@section('title','主页')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>你好，朋友！</h1>
+    <p class="lead">
+      欢迎来到心情驿站
+    </p>
+    <p>
+      新的一刻将从这里开始.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href=" {{route('signup')}} " role="button">现在注册</a>
+    </p>
+  </div>
 @stop
